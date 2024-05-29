7717b6f3c1ff239997f4e7496ac3b30ef8c3a2f9
Doug Mamilor <dougmamilor@icloud.com>
2024-05-29 17:13:30 -0400

    add vimtex

--- init.lua
+++ init.lua
@@ -301,6 +301,21 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here
+      vim.g.tex_flavor = 'latex'
+      vim.g.vimtex_view_method = 'skim'
+      vim.g.vimtex_compiler_progname = 'nvr'
+      vim.g.vimtex_compiler_latexmk = {
+        continuous = 1,
+      }
+    end,
+  },
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
