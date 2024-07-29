d8fc2746f5df834a64d746d305a4407cc7a864be
Doug Mamilor <dougmamilor@icloud.com>
2024-07-29 18:19:57 -0400

    fix rust_analyzer issue

--- init.lua
+++ init.lua
@@ -711,6 +711,7 @@ require('lazy').setup({
             if server_name == 'rust_analyzer' then
               return
             end
+
             local server = servers[server_name] or {}
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
