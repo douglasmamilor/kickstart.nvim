74ee061dfb356bfa8f79c413f5fd60ea398aa6d4
Doug Mamilor <dougmamilor@icloud.com>
2024-08-12 11:52:20 -0400

    use tokyonight and add Trouble plugin

--- init.lua
+++ init.lua
@@ -891,58 +891,95 @@ require('lazy').setup({
   },
 
   -- Set theme to nightfox
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'EdenEast/nightfox.nvim',
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'EdenEast/nightfox.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   config = function()
+  --     require('nightfox').setup {
+  --       palettes = {
+  --         -- Custom carbonfox with black background
+  --         carbonfox = {
+  --           bg1 = '#000000', -- Black background
+  --         },
+  --       },
+  --     }
+  --     -- Set the colorscheme to carbonfox
+  --     vim.cmd.colorscheme 'carbonfox'
+  --   end,
+  --   init = function()
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'carbonfox'
+  --
+  --     -- You can configure highlights by doing something like:
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
+
+  -- Or set theme to tokyonight
+  {
+    'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      require('nightfox').setup {
-        palettes = {
-          -- Custom carbonfox with black background
-          carbonfox = {
-            bg1 = '#000000', -- Black background
-          },
+      require('tokyonight').setup {
+        style = 'night', -- Ensure you're using the 'night' style of tokyonight
+        styles = {
+          sidebars = 'transparent',
+          floats = 'transparent',
         },
+        on_colors = function(colors)
+          colors.bg = '#000000' -- Set the background to black
+        end,
       }
-      -- Set the colorscheme to carbonfox
-      vim.cmd.colorscheme 'carbonfox'
-    end,
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'carbonfox'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      -- Set the colorscheme to tokyonight
+      vim.cmd.colorscheme 'tokyonight'
     end,
   },
 
-  -- Or set theme to tokyonight
-  -- {
-  --   'folke/tokyonight.nvim',
-  --   priority = 1000, -- Make sure to load this before all the other start plugins.
-  --   config = function()
-  --     require('tokyonight').setup {
-  --       style = 'night', -- Ensure you're using the 'night' style of tokyonight
-  --       styles = {
-  --         sidebars = 'transparent',
-  --         floats = 'transparent',
-  --       },
-  --       on_colors = function(colors)
-  --         colors.bg = '#000000' -- Set the background to black
-  --       end,
-  --     }
-  --     -- Set the colorscheme to tokyonight
-  --     vim.cmd.colorscheme 'tokyonight'
-  --   end,
-  -- },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
+  {
+    'folke/trouble.nvim',
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+    },
+  },
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
